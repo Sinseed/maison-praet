4219d7d68e582f3d8239ff6fba82abb6ce2cdfaa
Thomas Praet <thomas@maisonpraet.ch>
2026-04-07 14:05:53 +0000

    Add Senarclens photos (13 photos, vendu)

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ const MANDATS: Mandat[] = [
   { id: 11, titre: 'Appartement', lieu: 'Gland', prix: "1'250'000", pieces: '-', surface: '-', terrain: '-', categorie: 'partage', img: '/photos/gland-aubepine/DJI_20260115163614_0002_D.jpg' },
   { id: 12, titre: 'Appartement', lieu: 'Longirod', prix: "795'000", pieces: '-', surface: '-', terrain: '-', categorie: 'partage', img: '/photos/longirod/DJI_20260119103256_0010_D.jpg' },
   // Ventes conclues
-  { id: 13, titre: 'Maison individuelle', lieu: 'Senarclens', prix: "1'500'000", pieces: '-', surface: '-', terrain: '-', categorie: 'vendu', img: '' },
+  { id: 13, titre: 'Maison individuelle', lieu: 'Senarclens', prix: "1'500'000", pieces: '-', surface: '-', terrain: '-', categorie: 'vendu', img: '/photos/senarclens/1775570719205_IMG_5840.jpg' },
   { id: 14, titre: 'Immeuble 3 logements', lieu: 'Lausanne', prix: "1'480'000", pieces: '-', surface: '-', terrain: '-', categorie: 'vendu', img: '/photos/lausanne-monttendre/IMG_6530.jpg' },
 ]
 
